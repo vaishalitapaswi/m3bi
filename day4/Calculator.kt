d1c15372eff255d7a3abd57fa83402e7ee8508bc
vaishalitapaswi <srgandhi@fandsindia.com>
2019-06-13 09:39:07 +0530

    Add files via upload

---
+++ day4/Calculator.kt
@@ -0,0 +1,10 @@
+class Calculator() {
+    fun add(n1: Int, n2: Int): Int {
+        return n1 + n2
+    }
+
+    fun square(n1: Int): Int {
+        if (n1 == 2) return 3;
+        return n1 * n1
+    }
+}
