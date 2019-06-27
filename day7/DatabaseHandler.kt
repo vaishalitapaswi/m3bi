0630fd15d286fa1b48578adc357aba494be56faf
vaishalitapaswi <28345849+vaishalitapaswi@users.noreply.github.com>
2019-06-27 07:19:09 +0530

    Create DatabaseHandler.kt

---
+++ day7/DatabaseHandler.kt
@@ -0,0 +1,44 @@
+package com.example.anddemo
+
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.content.ContentValues
+import android.content.Context
+
+class DatabaseHandler(context: Context): SQLiteOpenHelper(context,DATABASE_NAME,null,DATABASE_VERSION) {
+    companion object {
+        private val DATABASE_VERSION = 1
+        private val DATABASE_NAME = "EmployeeDatabase"
+        private val TABLE_CONTACTS = "EmployeeTable"
+        private val KEY_ID = "empno"
+        private val KEY_NAME = "ename"
+        private val KEY_SALARY = "salary"
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+
+        //creating table with fields
+        val CREATE_CONTACTS_TABLE = ("CREATE TABLE " + TABLE_CONTACTS + "("
+                + KEY_ID + " INTEGER PRIMARY KEY," + KEY_NAME + " TEXT,"
+                + KEY_SALARY + " INTEGER" + ")")
+        db?.execSQL(CREATE_CONTACTS_TABLE)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db!!.execSQL("DROP TABLE IF EXISTS " + TABLE_CONTACTS)
+        onCreate(db)
+    }
+
+
+    //method to insert data
+    fun addEmployee(emp: Emp): Long {
+        val db = this.writableDatabase
+        val contentValues = ContentValues()
+        contentValues.put(KEY_ID, emp.empno)
+        contentValues.put(KEY_NAME, emp.ename)
+        contentValues.put(KEY_SALARY, emp.salary)
+        val success = db.insert(TABLE_CONTACTS, null, contentValues)
+        db.close()
+        return success
+    }
+}
